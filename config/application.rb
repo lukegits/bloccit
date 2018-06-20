2a78db9a42cc5bdbf6ccbf3a7788a38f592aef80
Lukegits <luket.chavez@gmail.com>
2018-06-19 19:08:12 -0600

    seeding checkpoint work

--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,7 @@ module Bloccit
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
+    config.autoload_paths << File.join(config.root, "lib")
     # the framework and any gems in your application.
 
     # Don't generate system test files.

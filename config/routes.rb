83281e5f7d19125c564e6ca39686f97c9551ed9f
Lukegits <luket.chavez@gmail.com>
2018-06-30 08:35:16 -0600

    topics and posts 630 835am

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,6 @@ Rails.application.routes.draw do
     resources :sponsored_posts, except: [:index]
   end
   get 'about' => 'welcome#about'
+  get "/welcome/faq" => "welcome#faq"
   root to: 'welcome#index'
 end

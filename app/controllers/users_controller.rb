ba1dc2cb8f50bbb0239b5c0417ade6adae0514bf
Lukegits <luket.chavez@gmail.com>
2018-07-03 12:15:05 -0600

    sign up form assignment work 73 1215pm

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,8 @@
 class UsersController < ApplicationController
   def new
-     @user = User.new
-   end
-   def user_params
-   params.require(:user).permit(:email, :password, :password_confirmation)
-   end
-   helper_method :user_params
-   end
+    @user = User.new
+  end
+  helper_method :user_params
    def confirm
     @user = User.new
     @user.name = params[:user][:name]
@@ -14,15 +10,14 @@ class UsersController < ApplicationController
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
   end
-   def create
-
+  def create
     @user = User.new
     @user.name = params[:user][:name]
     @user.email = params[:user][:email]
     @user.password = params[:user][:password]
     @user.password_confirmation = params[:user][:password_confirmation]
 
-# #10
+
     if @user.save
       flash[:notice] = "Welcome to Bloccit #{@user.name}!"
       redirect_to root_path
@@ -31,3 +26,8 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   end
   # #7
   resources :users, only: [:new, :create]
+
   post 'users/confirm' => 'users#confirm'
 
   get 'about' => 'welcome#about'
